8d822c09a068c281dd351b1f5065190e564d0ffe
Lassi Kuisma <lassi.kuisma1@gmail.com>
2023-03-14 21:07:45 +0200

    example: fragments

--- example-phonebook/client/src/queries.js
+++ example-phonebook/client/src/queries.js
@@ -1,26 +1,33 @@
 import { gql } from '@apollo/client'
 
-export const ALL_PERSONS = gql`
-  query {
-    allPersons {
-      name
-      phone
-      id
+const PERSON_DETAILS = gql`
+  fragment PersonDetails on Person {
+    id
+    name
+    phone
+    address {
+      street
+      city
     }
   }
 `
+
+export const ALL_PERSONS = gql`
+  query {
+    allPersons {
+      ...PersonDetails
+    }
+  }
+  ${PERSON_DETAILS}
+`
+
 export const FIND_PERSON = gql`
   query findPersonByName($nameToSearch: String!) {
     findPerson(name: $nameToSearch) {
-      name
-      phone
-      id
-      address {
-        street
-        city
-      }
+      ...PersonDetails
     }
   }
+  ${PERSON_DETAILS}
 `
 export const CREATE_PERSON = gql`
   mutation createPerson(
