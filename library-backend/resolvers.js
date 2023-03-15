ab3bc8f90c59873570bb291c9ac792d99523645c
Lassi Kuisma <lassi.kuisma1@gmail.com>
2023-03-15 19:36:11 +0200

    resolvers and schema files

---
+++ library-backend/resolvers.js
@@ -0,0 +1,149 @@
+const { GraphQLError } = require('graphql')
+const jwt = require('jsonwebtoken')
+const Book = require('./models/book')
+const Author = require('./models/author')
+const User = require('./models/user')
+
+const resolvers = {
+  Query: {
+    bookCount: async () => Book.collection.countDocuments(),
+    authorCount: async () => Author.collection.countDocuments(),
+    allBooks: async (root, args) => {
+      const genre = args.genre
+      if (genre) {
+        return Book.find({ genres: genre })
+      }
+
+      // TODO: filter by author
+      return Book.find({})
+    },
+    allAuthors: async () => Author.find({}),
+    me: async (root, args, context) => {
+      return context.currentUser
+    },
+  },
+  Book: {
+    author: async (root) => Author.findById(root.author),
+  },
+  Author: {
+    bookCount: async ({ name }) => {
+      // TODO: bookCount on author
+      return 0
+    },
+  },
+  Mutation: {
+    addBook: async (root, args, { currentUser }) => {
+      if (!currentUser) {
+        throw new GraphQLError('not authenticated', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+          },
+        })
+      }
+
+      const getOrCreateAuthor = async (name) => {
+        const author = await Author.findOne({ name })
+        if (author) {
+          return author
+        }
+
+        const created = new Author({ name })
+        await created.save().catch((error) => {
+          throw new GraphQLError('Failed to create author', {
+            extensions: {
+              code: 'BAD_USER_INPUT',
+              invalidArgs: name,
+              error,
+            },
+          })
+        })
+
+        return created
+      }
+
+      const author = await getOrCreateAuthor(args.author)
+
+      const book = new Book({ ...args, author })
+      await book.save().catch((error) => {
+        throw new GraphQLError('Failed to save book info', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.title,
+            error,
+          },
+        })
+      })
+
+      return book
+    },
+    editAuthor: async (root, args, { currentUser }) => {
+      if (!currentUser) {
+        throw new GraphQLError('not authenticated', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+          },
+        })
+      }
+
+      const author = await Author.findOne({ name: args.name })
+      if (!author) {
+        throw new GraphQLError('Author not found', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.name,
+          },
+        })
+      }
+
+      author.born = Number(args.setBornTo)
+
+      author.save().catch((error) => {
+        throw new GraphQLError('Saving author failed', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.name,
+            error,
+          },
+        })
+      })
+
+      return author
+    },
+    createUser: async (root, args) => {
+      const user = new User({
+        username: args.username,
+        favouriteGenre: args.favouriteGenre,
+      })
+
+      return user.save().catch((error) => {
+        throw new GraphQLError('Creating the user failed', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.name,
+            error,
+          },
+        })
+      })
+    },
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username })
+
+      if (!user || args.password !== 'secret') {
+        throw new GraphQLError('wrong credentials', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+          },
+        })
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id,
+      }
+
+      return { value: jwt.sign(userForToken, process.env.JWT_SECRET) }
+    },
+  },
+}
+
+module.exports = resolvers
